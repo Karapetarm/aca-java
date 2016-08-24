01aab8af3c8d448935938e751288f78d7f00af39
Karapetarm <karapetjava@gmail.com>
2016-08-24 16:47:23 +0400

    a

---
+++ Lessen 1708/src/gcard/system/DataBase.java
@@ -0,0 +1,16 @@
+package gcard.system;
+
+import gcard.cadr.UserCard;
+import gcard.terminal.Terminal;
+import gcard.user.User;
+
+import java.util.List;
+
+
+public class DataBase {
+
+   public static List<UserCard> cards;
+   public static List<User> registerUser;
+   public static List<Terminal> terminalList;
+   public static List<List<String>> transactionList;
+}
---
+++ Lessen 1708/src/gcard/terminal/StoreTerminal.java
@@ -0,0 +1,41 @@
+package gcard.terminal;
+
+import gcard.cadr.UserCard;
+import gcard.system.DataBase;
+
+import java.util.UUID;
+
+
+public class StoreTerminal extends Terminal {
+
+    public StoreTerminal(UUID id, String address) {
+        super(id, address);
+    }
+
+    @Override
+    public void receivePayment(UUID cardId, int amount) {
+        A:
+        {
+            for (UserCard userCard :  DataBase.cards) {
+                if (userCard.getId().equals(cardId)) {
+                    if (!userCard.getIsStolen()) {
+                        if (userCard.getBalance() - amount >= 0) {
+                            userCard.doPayment(amount);
+                            System.out.println("Transaction Succeed");
+                            break A;
+                        } else {
+                            System.out.println("Insufficient balance ");
+                            break A;
+                        }
+                    } else {
+                        System.out.println("Card Is Blocked Or Stolen");
+                        break A;
+                    }
+                }
+            }
+            System.out.println("Card NOT Found in Database");
+        }
+    }
+
+
+}
